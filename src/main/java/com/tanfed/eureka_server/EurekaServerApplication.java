53d4f10cbd4889d3254e5dbfc1bf245cc1b8dcdd
TANFEDIT <tanfeditteam@gmail.com>
2025-09-03 19:41:00 +0530

    test api

--- src/main/java/com/tanfed/eureka_server/EurekaServerApplication.java
+++ src/main/java/com/tanfed/eureka_server/EurekaServerApplication.java
@@ -3,10 +3,18 @@ package com.tanfed.eureka_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
+@RestController
 public class EurekaServerApplication {
+	
+	@GetMapping("/test")
+	public String test() throws Exception {
+		return "Service Deployed Successfully!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
